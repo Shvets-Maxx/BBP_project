50f758c5da59ad7f441e703d2b1cfc06743904b9
Max <101049443+Shvets-Maxx@users.noreply.github.com>
2025-07-15 19:20:15 +0300

    fix bags with dayjs

--- src/features/profile/W-Admin-Services-Order-for-items-order-view/Time/Time.tsx
+++ src/features/profile/W-Admin-Services-Order-for-items-order-view/Time/Time.tsx
@@ -1,21 +1,77 @@
+// import { DemoContainer, DemoItem } from "@mui/x-date-pickers/internals/demo";
+// import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+// import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+// import { TimePicker } from "@mui/x-date-pickers/TimePicker";
+// import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+// // import dayjs from "dayjs";
+// import dayjs, { Dayjs } from "dayjs";
+// import { useState } from "react";
+
+// import style from "../Time/Time.module.sass";
+
+// export default function TimePickerViews({ disabled }: { disabled: boolean }) {
+// 	const [date, setDate] = useState<Dayjs | null>(dayjs());
+// 	const [time, setTime] = useState<Dayjs | null>(dayjs());
+
+// 	return (
+// 		<div className={style.time}>
+// 			<LocalizationProvider dateAdapter={AdapterDayjs}>
+// 				<DemoContainer components={["TimePicker", "TimePicker", "DatePicker"]}>
+// 					<div
+// 						className={style["time__picker-block"]}
+// 						style={{ background: disabled ? "none" : "#EBEBED" }}
+// 					>
+// 						<DemoItem>
+// 							<DatePicker
+// 								onChange={(value, _context) => {
+// 									setDate(value as Dayjs | null);
+// 								}} // onChange={(newValue) => setDate(newValue as unknown as Dayjs)}
+// 								disabled={disabled}
+// 								value={date}
+// 								// defaultValue={dayjs("2022-04-17")}
+// 							/>
+// 						</DemoItem>{" "}
+// 					</div>
+// 					<div className={style.time__text}>
+// 						<p>at</p>
+// 					</div>
+// 					<div
+// 						className={style["time__picker-block"]}
+// 						style={{ background: disabled ? "none" : "#EBEBED" }}
+// 					>
+// 						<DemoItem>
+// 							<TimePicker
+// 								value={time}
+// 								onChange={(value, _context) => {
+// 									setTime(value as Dayjs | null);
+// 								}} // onChange={(newValue) => setTime(newValue as unknown as Dayjs)}
+// 								disabled={disabled}
+// 								views={["hours", "minutes"]}
+// 							/>
+// 						</DemoItem>
+// 					</div>
+// 				</DemoContainer>
+// 			</LocalizationProvider>
+// 		</div>
+// 	);
+// }
+
 import { DemoContainer, DemoItem } from "@mui/x-date-pickers/internals/demo";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { TimePicker } from "@mui/x-date-pickers/TimePicker";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
-// import dayjs from "dayjs";
-import dayjs, { Dayjs } from "dayjs";
 import { useState } from "react";
 
 import style from "../Time/Time.module.sass";
 
 export default function TimePickerViews({ disabled }: { disabled: boolean }) {
-	const [date, setDate] = useState<Dayjs | null>(dayjs());
-	const [time, setTime] = useState<Dayjs | null>(dayjs());
+	const [date, setDate] = useState<Date | null>(new Date());
+	const [time, setTime] = useState<Date | null>(new Date());
 
 	return (
 		<div className={style.time}>
-			<LocalizationProvider dateAdapter={AdapterDayjs}>
+			<LocalizationProvider dateAdapter={AdapterDateFns}>
 				<DemoContainer components={["TimePicker", "TimePicker", "DatePicker"]}>
 					<div
 						className={style["time__picker-block"]}
@@ -23,14 +79,11 @@ export default function TimePickerViews({ disabled }: { disabled: boolean }) {
 					>
 						<DemoItem>
 							<DatePicker
-								onChange={(value, _context) => {
-									setDate(value as Dayjs | null);
-								}} // onChange={(newValue) => setDate(newValue as unknown as Dayjs)}
+								onChange={(value) => setDate(value)}
 								disabled={disabled}
 								value={date}
-								// defaultValue={dayjs("2022-04-17")}
 							/>
-						</DemoItem>{" "}
+						</DemoItem>
 					</div>
 					<div className={style.time__text}>
 						<p>at</p>
@@ -42,9 +95,7 @@ export default function TimePickerViews({ disabled }: { disabled: boolean }) {
 						<DemoItem>
 							<TimePicker
 								value={time}
-								onChange={(value, _context) => {
-									setTime(value as Dayjs | null);
-								}} // onChange={(newValue) => setTime(newValue as unknown as Dayjs)}
+								onChange={(value) => setTime(value)}
 								disabled={disabled}
 								views={["hours", "minutes"]}
 							/>
